086ebd45d9a578929e7fce5e952f459976a3eba7
Qmi <minh.pham1126@gmail.com>
2024-05-25 10:06:49 +0700

    update vim_opts

--- lua/vim_opts.lua
+++ lua/vim_opts.lua
@@ -1,23 +1,63 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
 vim.g.mapleader = " "
 vim.g.background = "light"
 
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
 vim.opt.relativenumber = true
-vim.keymap.set('n','<C-cdr>', ':cd %:p:h<CR>')
-vim.cmd [[
+vim.keymap.set("n", "<C-cdr>", ":cd %:p:h<CR>")
+vim.cmd([[
   command! CDC cd %:p:h
-]]
+]])
 
+-- lsp during input
+vim.diagnostic.config({ update_in_insert = true })
+
+-- greatest keymap ever
+vim.keymap.set("x", "<leader>p", '"_dP')
