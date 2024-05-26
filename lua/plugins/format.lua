76367c12ecff686c2a52ac57be44439995cf1250
Qmi <minh.pham1126@gmail.com>
2024-05-26 15:16:16 +0700

    fix some format issues and add rust lsp/formatter

---
+++ lua/plugins/format.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "lukas-reineke/lsp-format.nvim",
+    config = function()
+      require("lsp-format").setup({})
+      require("lspconfig").gopls.setup({ on_attach = require("lsp-format").on_attach })
+    end,
+  },
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,6 +23,30 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+      local util = require("lspconfig.util")
+      local on_attach = function(client, bufnr)
+        local function buf_set_keymap(...)
+          vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+        local function buf_set_option(...)
+          vim.api.nvim_buf_set_option(bufnr, ...)
+        end
+
+        buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+        local opts = { noremap = true, silent = true }
+
+        buf_set_keymap("n", "gD", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+        buf_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+        buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+        buf_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+        buf_set_keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+        buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+        buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+        buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+        buf_set_keymap("n", "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", opts)
+        buf_set_keymap("n", "<leader>lR", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        client.server_capabilities.document_formatting = true
+      end
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
@@ -34,6 +58,19 @@ return {
       })
       lspconfig.emmet_language_server.setup({
         capabilities = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        filetypes = { "rust" },
+        root_dir = util.root_pattern("Cargo.toml"),
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+          },
+        },
       })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
