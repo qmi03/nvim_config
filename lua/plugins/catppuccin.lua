b60a617764af84fb571a6452ec9ce70a319e396f
Qmi <minh.pham1126@gmail.com>
2024-05-26 15:13:57 +0700

    update theme to use terminal background image

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,12 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+      vim.api.nvim_command("hi Normal guibg=NONE ctermbg=NONE")
+    end,
+  },
 }
