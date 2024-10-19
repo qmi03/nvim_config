60d851f6321b870d1ac4956aaca678dca28dedbb
Qmi <minh.pham1126@gmail.com>
2024-10-19 20:29:44 +0700

    remove: catppuccin until i figure out my wezterm config to work with catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,15 +1,15 @@
-return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		integrations = {
-			nvimtree = true,
-		},
-		config = function()
-			vim.cmd.colorscheme("catppuccin")
-			vim.api.nvim_command("hi Normal guibg=NONE ctermbg=NONE")
-		end,
-	},
-}
+return {} -- return {
+-- 	{
+-- 		"catppuccin/nvim",
+-- 		lazy = false,
+-- 		name = "catppuccin",
+-- 		priority = 1000,
+-- 		integrations = {
+-- 			nvimtree = true,
+-- 		},
+-- 		config = function()
+-- 			vim.cmd.colorscheme("catppuccin")
+-- 			vim.api.nvim_command("hi Normal guibg=NONE ctermbg=NONE")
+-- 		end,
+-- 	},
+-- }
