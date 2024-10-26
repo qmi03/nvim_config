d374f9e1e99d56df8b35292c0d8a4d0e624a02dc
Qmi <minh.pham1126@gmail.com>
2024-10-26 11:07:28 +0700

    change: add nix format and remove black+isort

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -12,10 +12,8 @@ return {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.rubocop,
 					null_ls.builtins.diagnostics.rubocop,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
 					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.formatting.nixpkgs_fmt
+					null_ls.builtins.formatting.nixpkgs_fmt,
 				},
 			})
 
